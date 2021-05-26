826b1d94d2c1af6fe75c4d2cb5833796a03ace6d
navyapp <79969396+navyapp@users.noreply.github.com>
2021-05-26 16:42:04 +0530

    program no 9

---
+++ FIRST 15 PROGRAMS./size.java
@@ -0,0 +1,24 @@
+
+public class size
+{
+public static void main(String[]args)
+{
+int a = 42;
+switch(a)
+{
+case 29:System.out.println("small");
+break;
+case 42:System.out.println("medium");
+break;
+case 44:System.out.println("large");
+break;
+case 48:System.out.println("extra large");
+break;
+default:
+System.out.println("unknown");
+break;
+}
+
+
+}
+}
