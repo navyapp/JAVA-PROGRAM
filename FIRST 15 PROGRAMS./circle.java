3a489994a15928f18e2f6b0fd7db748f9037f310
navyapp <79969396+navyapp@users.noreply.github.com>
2021-05-26 12:52:56 +0530

    prgram no 4

---
+++ FIRST 15 PROGRAMS./circle.java
@@ -0,0 +1,15 @@
+
+public class circle
+{
+public static void main(String[]args)
+{
+
+double area,perimeter;
+area=Math.PI*7.5*7.5;
+perimeter=2*Math.PI*7.5;
+System.out.println("Area of a circle is :"+area );
+System.out.println("Perimeter of  circle is :"+perimeter);
+
+
+}
+}
