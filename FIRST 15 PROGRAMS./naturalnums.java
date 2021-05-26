00adef89b43ca9f6c1cec764b5b25045cd3a355d
navyapp <79969396+navyapp@users.noreply.github.com>
2021-05-26 13:27:42 +0530

    program no 7

---
+++ FIRST 15 PROGRAMS./naturalnums.java
@@ -0,0 +1,14 @@
+
+public class naturalnums
+{
+public static void main(String[]args)
+{
+int i;
+System.out.println("first 10 natural numbers is:");
+for(i=1;i<=10;i++)
+{
+System.out.println(i);
+}
+
+}
+}
