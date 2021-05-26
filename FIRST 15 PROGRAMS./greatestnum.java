8535683ecf53efac9fdb9ce2e940edf22a4d966e
navyapp <79969396+navyapp@users.noreply.github.com>
2021-05-26 13:12:59 +0530

    program no 6

---
+++ FIRST 15 PROGRAMS./greatestnum.java
@@ -0,0 +1,14 @@
+
+public class greatestnum
+{
+public static void main(String[]args)
+{
+int a=5,b=10,c=20;
+if(a>b)
+System.out.println("a is greatest number");
+else if(b>c)
+System.out.println("b is greatest number");
+else
+System.out.println("c is greatest number");
+}
+}
