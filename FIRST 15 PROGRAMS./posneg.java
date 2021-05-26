34e8acd7acd6e4bb8249acd3c3e1545626cebf3a
navyapp <79969396+navyapp@users.noreply.github.com>
2021-05-26 13:04:51 +0530

    program no 5

---
+++ FIRST 15 PROGRAMS./posneg.java
@@ -0,0 +1,13 @@
+
+public class posneg
+{
+public static void main(String[]args)
+{
+int a=56;
+if(a>=0)
+System.out.println("a is a positive number");
+else
+System.out.println("a is a negative number");
+
+}
+}
