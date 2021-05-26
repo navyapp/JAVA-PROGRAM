e099b1a5190154be62102f785109f0cca85ddfae
navyapp <79969396+navyapp@users.noreply.github.com>
2021-05-26 13:41:07 +0530

    program no 8

---
+++ FIRST 15 PROGRAMS./sumavg.java
@@ -0,0 +1,14 @@
+
+public class sumavg
+{
+public static void main(String[]args)
+{
+int a=5,b=10,c=15,d=20,e=25,sum, avg;
+sum=a+b+c+d+e;
+avg=sum/5;
+System.out.println("sum of above 5 numbers is  :"+sum);
+System.out.println("avarage is : "+avg);
+
+
+}
+}
