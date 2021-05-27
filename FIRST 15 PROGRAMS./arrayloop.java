700709ee5abe1f1b06c380acb9bb287f675bac42
navyapp <79969396+navyapp@users.noreply.github.com>
2021-05-27 09:46:49 +0530

    program no 12

---
+++ FIRST 15 PROGRAMS./arrayloop.java
@@ -0,0 +1,40 @@
+public class arrayloop
+{
+public static void main(String[]args)
+{
+int a[]=new int[4];
+a[0]=1;
+a[1]=2;
+a[2]=4;
+a[3]=5;
+for(int i=0;i<a.length;i++)
+{
+System.out.println(a[i]);
+}
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
