934e90ec6c3b9e0b6f3d2105766292140f71bb3a
navyapp <79969396+navyapp@users.noreply.github.com>
2021-05-27 09:48:24 +0530

    program no 13

---
+++ FIRST 15 PROGRAMS./arrayy.java
@@ -0,0 +1,40 @@
+public class arrayy
+{
+public static void main(String[]args)
+{
+int a[]=new int[4];
+a[0]=1;
+a[1]=2;
+a[2]=4;
+a[3]=5;
+for(int b:a)
+{
+System.out.println(b);
+}
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
