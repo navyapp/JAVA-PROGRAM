6757ae0265ecd4381fee239c1cd58bd5ad3a525f
navyapp <79969396+navyapp@users.noreply.github.com>
2021-05-27 09:45:01 +0530

    program no 11

---
+++ FIRST 15 PROGRAMS./array.java
@@ -0,0 +1,43 @@
+
+public class array
+{
+public static void main(String[]args)
+{
+int a[]=new int[4];
+ a[0]=12;
+ a[1]=4;
+ a[2]=5;
+ a[3]=2;
+System.out.println("FIRST ELEMENT:"+a[0]);
+System.out.println("SECOND ELEMENT:"+a[1]);
+System.out.println("THIRD ELEMENT:"+a[2]);
+System.out.println("FOURTH ELEMENT:"+a[3]);
+
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
