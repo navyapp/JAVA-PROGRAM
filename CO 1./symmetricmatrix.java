cadb802a04b516705702bd0035cd81cac6588866
navyapp <79969396+navyapp@users.noreply.github.com>
2021-08-25 00:23:21 +0530

    program no 4

--- CO 1./symmetricmatrix.java
+++ CO 1./symmetricmatrix.java
@@ -1,3 +1,4 @@
+Q4:Read a matrix from the console and check whether it is symmetric or not.
 import java.util.Scanner;
 
 class symmetricmatrix
@@ -93,3 +94,25 @@ class symmetricmatrix
  }
 
 }   
+
+OUTPUT
+
+D:\JAVA PROGRAMS>javac symmetricmatrix.java
+
+D:\JAVA PROGRAMS>java symmetricmatrix
+enter the rows
+3
+enter the columes
+2
+enter the elements
+2
+3
+4
+5
+6
+7
+enter the matrix
+2       3
+4       5
+6       7
+the matrix in not a square matrix
