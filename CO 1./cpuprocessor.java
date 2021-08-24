d353fc6d4cb9da95db689046a9c49ea9d1d9cd8d
navyapp <79969396+navyapp@users.noreply.github.com>
2021-08-25 00:37:34 +0530

    program no 5

--- CO 1./cpuprocessor.java
+++ CO 1./cpuprocessor.java
@@ -1,3 +1,6 @@
+Q5:Create CPU with attribute price. Create inner class Processor (no. of cores, manufacturer) 
+and static nested class RAM (memory, manufacturer). Create an object of CPU and print 
+information of Processor and RAM. 
 
 class CPU
 {
@@ -24,7 +27,18 @@ class cpuprocessor
   CPU.RAM ram=cpu.new RAM();
   System.out.println("processor details:\n" + processor.cores +  processor.manifacture);
   System.out.println("RAM details:\n" + ram.memory + ram.manifacture);
-  System.out.println("cpu price:\t"+cpu.price);
+  System.out.println("cpu price:"+cpu.price);
   
   }
 }
+
+OUTPUT
+
+D:\JAVA PROGRAMS>javac cpuprocessor.java
+
+D:\JAVA PROGRAMS>java cpuprocessor
+processor details:
+5 samsung
+RAM details:
+124 intel
+cpu price: 5000
