7462a69ded2b1bed88b278730bc3ee57dc4bfb3b
navyapp <79969396+navyapp@users.noreply.github.com>
2021-08-25 00:43:57 +0530

    program no 1

--- CO 2./SortingString.java
+++ CO 2./SortingString.java
@@ -1,3 +1,6 @@
+
+Q1:Program to Sort strings 
+
 import java.util.Arrays;
 import java.util.Scanner;
 public class SortingString
@@ -17,3 +20,14 @@ public class SortingString
       System.out.println("sorted string is:\n"+new String(charArray));
    }
 }
+
+OUTPUT
+
+
+D:\JAVA PROGRAMS>javac SortingString.java
+
+D:\JAVA PROGRAMS>java SortingString
+Enter a string value:
+PYTHON
+sorted string is:
+HNOPTY
