d7d870b37f1b12dd1d3a0c0dc7aafb8b044b2adb
navyapp <79969396+navyapp@users.noreply.github.com>
2021-08-26 23:24:49 +0530

    program no 1

---
+++ CO 4./Area.java
@@ -0,0 +1,11 @@
+import Graphics.*;
+              
+public class Area{
+                 public static void main(String[] args){
+                                                        Figure f=new Figure();
+                                                        System.out.println("Area of Rectangle is :" +f.Rectangle(3.0,2.0));
+                                                        System.out.println("Area of Triangle is :" +f.Triangle(5.0,2.0));
+                                                        System.out.println("Area of Square is :" +f.Square(6.0));
+                                                        System.out.println("Area of Circle is :" +f.Circle(5.0));
+                                                       }
+}
