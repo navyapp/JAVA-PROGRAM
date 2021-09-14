68555992c0bf486a8a6ab0874ccfd1af11a8a6e4
navyapp <79969396+navyapp@users.noreply.github.com>
2021-09-15 00:31:56 +0530

    program no 5

--- CO 4./MThread.java
+++ CO 4./MThread.java
@@ -1,3 +1,6 @@
+Q 5:Define 2 classes; one for generating multiplication table of 5 and other for displaying first 
+N prime numbers. Implement using threads. (Thread class)
+
 import java.util.*;
 class Mtable extends Thread
 {
@@ -72,3 +75,33 @@ public class MThread
   obj2.start();
  }
 }
+
+OUTPUT
+
+D:\JAVA PROGRAMS>javac MThread.java
+
+D:\JAVA PROGRAMS>java MThread
+Enter the limit of a prime numbers:
+10
+Multiplication table of 5 is :
+1*5=5
+2*5=10
+3*5=15
+4*5=20
+5*5=25
+6*5=30
+7*5=35
+8*5=40
+9*5=45
+10*5=50
+primenumber upto10is:
+2
+3
+5
+7
+11
+13
+17
+19
+23
+29
